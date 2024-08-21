6d3703a1413aa92bb048d2ddddbbd7b7c535af41
PANdas <krsy0411@naver.com>
2024-08-21 18:59:49 +0900

    null 병합 연산자 추가해보기 test

--- workshop/backend/Program.cs
+++ workshop/backend/Program.cs
@@ -94,7 +94,7 @@ internal class YouTubeSummariserService(IYouTubeVideo youtube, AzureOpenAIClient
 
     public async Task<string> SummariseAsync(SummaryRequest req)
     {
-        Subtitle subtitle = await this._youtube.ExtractSubtitleAsync(req.YouTubeLinkUrl, req.VideoLanguageCode).ConfigureAwait(false);
+        Subtitle subtitle = await this._youtube.ExtractSubtitleAsync(req.YouTubeLinkUrl ?? " https://youtu.be/NN4Zzp-vOrU", req.VideoLanguageCode).ConfigureAwait(false);
         string caption = subtitle.Content.Select(p => p.Text).Aggregate((a, b) => $"{a}\n{b}");
 
         var chat = this._openai.GetChatClient(this._config["OpenAI:DeploymentName"]);
